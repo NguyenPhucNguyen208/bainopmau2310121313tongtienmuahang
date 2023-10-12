4cb79449805240549ab0930746744fc2618e4dab
NguyenPhucNguyen208 <85856596+NguyenPhucNguyen208@users.noreply.github.com>
2023-10-12 07:01:43 +0000

    Nop bai

--- s00_bailam.py
+++ s00_bailam.py
@@ -31,5 +31,8 @@ tongtienmuahang(None)                   | None
 
 #region bailam
 def tongtienmuahang(gia_truoc_thue):
-  return 'todo'
+  if gia_truoc_thue is None : 
+    return None 
+  tax = int(gia_truoc_thue)*(10/100)
+  return int(gia_truoc_thue)+int(tax)  
 #endregion bailam
